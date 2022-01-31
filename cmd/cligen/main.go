4cd5ff642009f58714f4405c8f6300637da7a9e6
Sasha Melentyev <sasha@melentyev.io>
2022-01-31 09:30:15 +0300

    feat: add entry point and add imports to tmpl

--- cmd/cligen/main.go
+++ cmd/cligen/main.go
@@ -1,4 +1,30 @@
 package main
 
+import (
+	"log"
+	"os"
+	"text/template"
+
+	"github.com/go-dummy/cligen"
+)
+
 func main() {
+	t, err := template.ParseFiles("./tmpl/acmd.tmpl")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	f, err := os.OpenFile("run.go", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	api, err := cligen.Parse("./cligen.yml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := t.Execute(f, api); err != nil {
+		log.Fatalln(err)
+	}
 }
