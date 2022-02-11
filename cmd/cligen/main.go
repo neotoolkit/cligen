9b74c3dbba547a5e9d2ea456878051566eae5019
m.sychev <m.w.s@bk.ru>
2022-02-11 10:28:39 +0300

    Fix generate empty file on prepare error

--- cmd/cligen/main.go
+++ cmd/cligen/main.go
@@ -14,19 +14,19 @@ func main() {
 
 	flag.Parse()
 
-	t := cligen.Template(cli)
-
-	f, err := os.OpenFile("run.go", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	api, err := cligen.Parse("./.cligen.yml")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	if err := cligen.Validate(api.Commands); err != nil {
+		log.Fatalln(err)
+	}
+
+	t := cligen.Template(cli)
+
+	f, err := os.OpenFile("run.go", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
 		log.Fatalln(err)
 	}
 
